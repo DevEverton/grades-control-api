aa7a858015b55bfb5fdb33c631f6e82c73657adf
Everton Carneiro <Everton@MacBook-Pro-de-Everton.local>
2020-06-08 18:55:27 -0300

    Created method POST

--- routes/grades.js
+++ routes/grades.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs").promises;
 const router = express.Router();
+let time = new Date();
 
 router.get("/", async (_, res) => {
   try {
@@ -10,6 +11,22 @@ router.get("/", async (_, res) => {
     res.send(json);
   } catch (err) {
     res.status(400).send({ error: err.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  let grade = req.body;
+  try {
+    let data = await fs.readFile(fileName, "utf8");
+    let json = JSON.parse(data);
+    grade = { id: json.nextId++, ...grade, timestamp: time.toISOString() };
+    json.grades.push(grade);
+    await fs.writeFile(fileName, JSON.stringify(json));
+    res.end();
+    //   logger.info(`POST /account - ${JSON.stringify(account)}`);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+    //   logger.error(`POST /account - ${err.message}`);
   }
 });
 
